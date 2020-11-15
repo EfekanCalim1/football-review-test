caf4f31a1a29ccbdcc4e98d17110475f5475ac95
EfekanCalim1 <ecalim@qa.com>
2020-11-15 20:57:38 +0000

    crud

--- Code/application/forms.py
+++ Code/application/forms.py
@@ -4,16 +4,6 @@ from wtforms.validators import DataRequired, ValidationError
 
 from application.models import Players, Review
 
-class PlayersCheck:
-    def __init__(self, message):
-        self.message = message
-
-    def __call__(self, form, field):
-        all_players = Players.query.all()
-        for players in all_players:
-            if players.name == field.data:
-                raise ValidationError(self.message)
-
 class ReviewCheck:
     def __init__(self, message):
         self.message = message
@@ -24,22 +14,10 @@ class ReviewCheck:
             if review.review == field.data:
                 raise ValidationError(self.message)
 
-class PlayersForm(FlaskForm):
-    name = StringField('Player Name',
-                validators=[
-                    DataRequired(),
-                    PlayersCheck(message= 'You have already added this player')
-                ]
-            )
-    submit = SubmitField('Add Player')
-
 class ReviewForm(FlaskForm):
     playerid = SelectField('Player',
                 choices=[]
                 )
-    rating = SelectField('Rating',
-                choices=[
-                    ('10', '10'), ('9', '9'), ('8', '8'), ('7', '7'), ('6', '6'), ('5', '5'), ('4', '4'), ('3', '3'), ('2', '2'), ('1', '1'), ('0', '0')])
     review = StringField('Review',
                 validators=[
                     DataRequired()])
