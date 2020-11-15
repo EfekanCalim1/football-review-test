ba56f799f91d4e92a5d6a18bb8e8233e7b99c262
EfekanCalim1 <ecalim@qa.com>
2020-11-15 23:06:35 +0000

    full crud functionality

--- Code/application/models.py
+++ Code/application/models.py
@@ -2,7 +2,7 @@ from application import db
 
 class Players(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    player = db.Column(db.String(30), nullable=False)
+    player = db.Column(db.String(255), nullable=False)
     review = db.relationship('Review', backref='players')
     
 class Review(db.Model):
